0b47cd70a8fab9c91d9fa2f9fc624197811016bf
jhumberto19 <31353808+jhumberto19@users.noreply.github.com>
2018-01-17 13:04:28 -0600

    2
    2

--- coleCC/config/routes.php
+++ coleCC/config/routes.php
@@ -49,7 +49,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Users', 'action' => 'login']);
+    $routes->connect('/', ['controller' => 'Users', 'action' => 'home']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
